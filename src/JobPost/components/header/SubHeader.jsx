e966ace7a32aab04f88ed55d8f355c4a4057c127
Pinki Jha <pinkijha1310@gmail.com>
2025-01-22 17:09:09 +0530

    add sub header on dashboard

--- src/JobPost/components/header/SubHeader.jsx
+++ src/JobPost/components/header/SubHeader.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { PiBellRinging } from "react-icons/pi";
 
 const SubHeader = () => {
   return (
-    <div>
-      
+    <div className='flex justify-between items-center md:ml-[100px] md:mr-[620px]'>
+      <div> 
+        <img src="src/assets/logo.png" alt="Logo" 
+         className="h-24 cursor-pointer mr-4" />
+        </div>
+        
+      <div className='flex items-center space-x-6'>
+      <PiBellRinging className='text-3xl' />
+      <button className='border-2 font-bold border-violet-700 border-solid rounded md:p-2 hover:bg-violet-700 hover:text-white
+       text-violet-700'>Post A Jobs</button>      
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/800px-Instagram_icon.png" alt="Logo" 
+         className="md:h-12 rounded-full cursor-pointer mr-4" />
+      </div>
     </div>
   )
 }
